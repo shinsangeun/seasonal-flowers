64ba61fed8b33c59d5ca9f183219e98b563c48c6
shinsangeun <s0813se@naver.com>
2021-05-01 18:38:15 +0900

    홈버튼 생성

--- seasonal-flowers/pages/season/index.js
+++ seasonal-flowers/pages/season/index.js
@@ -1,17 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
+import seasonStyles from '../../styles/season.module.css'
 
 export default function season() {
     return(
-        <div className={styles.container}>
-            <main className={styles.main}>
-                <h1 className={styles.title}>☀️ 계절 선택</h1>
-                <p className={styles.description}>
-                    원하시는 계절을 선택해 주세요!
-                    {/*<code className={styles.code}>pages/index.js</code>*/}
-                </p>
-            </main>
-        </div>
+        <>
+            <button className={seasonStyles.button}>홈으로..</button>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <h1 className={styles.title}>☀️ 계절 선택</h1>
+                    <p className={styles.description}>
+                        원하시는 계절을 선택해 주세요!
+                        {/*<code className={styles.code}>pages/index.js</code>*/}
+                    </p>
+                </main>
+            </div>
+        </>
     )
 }
